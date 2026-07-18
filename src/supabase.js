1575140af075d1b07788467a8ff7a6b3c7d9aa4a
Francisco Roa San Martin <franciscoroasanmartin@M9LKHX4F14.local>
2026-07-18 19:01:26 -0400

    fix supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://mftqwebckhhayhniaopo.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_25Bjrnb_SNR6Vx9JlOF_Kg_LJhr8uiU';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mdHF3ZWJja2hoYXlobmlhb3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ0MDEzOTQsImV4cCI6MjA5OTk3NzM5NH0.48KEbmcDgmAlzDNo_PhEEYD7ZDlQrWU8JFlGZSdOF7I';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
